4e861a0678cdb1127fec446a801d72f79c832012
bayberryy <yvonne881881@yahoo.com.sg>
2022-06-19 13:04:14 +0800

    firebase
    ->added firebase.py
    ->changed profile.html & routes.py
    now profile can be updated

--- public/routes.py
+++ public/routes.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from sympy import reduce_inequalities
+from firebase import *
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,6 +19,29 @@ def contact_page():
 @app.route('/gallery')
 def gallery_page():
     return render_template('gallery.html')
+
+@app.route('/notification')
+def notification_page():
+    return render_template('notification.html')
+
+@app.route('/profile', methods=['POST', 'GET'])
+def profile_page():
+    if request.method == "POST":
+        data = request.form
+        username = data['username']
+        profile_info = {}
+        profile_info = {
+            'name' : data['name'],
+            'email' : data['email'],
+            'occupancy' : data['occupancy']
+        }
+        check = db.child('users').child(username).get()
+        if check.val() != None:
+            db.child('users').child(username).update(profile_info)
+        else:
+            print('No data updated')
+        
+    return render_template('profile.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

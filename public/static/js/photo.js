254ca50df81d0b8959c88518e2178949769e052d
ellayang0618 <66121210+ellayang0618@users.noreply.github.com>
2022-07-26 19:17:45 +0800

    Minor edits

--- public/static/js/photo.js
+++ public/static/js/photo.js
@@ -6,19 +6,26 @@ function loadGalleryPage(){
 
 //An asynchronous function to fetch data from the API.
 async function sendApiRequest(){
-    API_KEY = 'Nf9R0l8xs8r7ZgCFF5rNTaaxQFRAIUcvEP454sEn'
+    API_KEY = 'R5tVXdeOHcZbmXMTPknErP3hUNKdyfTxeQV1ZueN'
     let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
     console.log(response)
     let data = await response.json()
     console.log(data)
+   
     useApiData(data)
+
 }
 
 
-//function that does something with the data received from the API. The name of the function should be customized to whatever you are doing with the data
 function useApiData(data){
     document.getElementById('title1').innerHTML = `<h1>${data.title}</h1>`;
     document.getElementById('content1').innerText = data.explanation;
-    document.getElementById('image1').innerHTML = `<img class="img-fluid" width="350" height="250" src="${data.url}" alt="">`;
+    if (data.media_type == 'image'){
+        
+        document.getElementById('source').innerHTML = `<img class="img-fluid" width="350" height="250" src="${data.url}" alt="">`;
+    }else{
+        document.getElementById('source').innerHTML = `<iframe width="500" height="350" src = "${data.url}"></iframe>`
+    }
+    
     document.getElementById('date1').innerText = data.date;
 }

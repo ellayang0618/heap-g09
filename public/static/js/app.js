d1bd79707909c2d4ffda92e37969c343d15e1143
bayberryy <yvonne881881@yahoo.com.sg>
2022-07-27 21:44:18 +0800

    updated articles

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -1,61 +1,69 @@
 $(document).ready(function () {
 
-    let url = "https://newsapi.org/v2/top-headlines?q=space&apiKey=e1952da524ab4e6e9eeccd8bee08f3f3";
-  
-    $.ajax({
-      url: url,
-      method: "GET",
-      dataType: "JSON",
-  
-      beforeSend: function () {
-        $(".progress").show();
-      },
-  
-      complete: function () {
-        $(".progress").hide();
-      },
-  
-      success: function (newsdata) {
-        let output = "";
-        let latestNews = newsdata.articles;
-        for (var i in latestNews) {
-          output += `
-            <div class="col l4 m6 s12">
-            <div class="card medium hoverable">
-              <div class="card-image">
-                <img src="${latestNews[i].urlToImage}" class="responsive-img" alt="${latestNews[i].title}">
+  let url = "https://newsapi.org/v2/top-headlines?q=space&apiKey=e1952da524ab4e6e9eeccd8bee08f3f3";
+
+  $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "JSON",
+
+    beforeSend: function () {
+      $(".progress").show();
+    },
+
+    complete: function () {
+      $(".progress").hide();
+    },
+
+    success: function (newsdata) {
+      let output = "";
+      let count = 0;
+      let latestNews = newsdata.articles;
+      for (var i in latestNews) {
+        if(count == 0){
+          output += `<div class="container p-4">
+          <div class="row">
+          <div class="card-columns d-flex justify-content-center">`
+          
+
+        };
+        output += `
+              <div class="col-lg-6 mb-4">
+                <div class="card" style="width: 27rem;">
+                    <img class="card-img-top" src="${latestNews[i].urlToImage}" alt="${latestNews[i].title}">
+                    <div class="card-body">
+                      <h5 class="card-title">Title: <a href="${latestNews[i].url}" title="${latestNews[i].title}">${latestNews[i].title}</a></h5>
+                      <h6 class="card-title">Author: ${latestNews[i].author}</h6>
+                      <h6 class="card-title">News source: ${latestNews[i].source.name}</h6>
+                      <h6 class="card-title">Published: ${latestNews[i].publishedAt}</h6>
+                      <h6 class="card-title">Description: </h6>
+                      <p class="card-text">${latestNews[i].description}</p>
+                      <a href="${latestNews[i].url}" class="btn btn-warning">Read More</a>
+                    </div>
+                </div>
               </div>
-              <div class="card-content">
-                <span class="card-title activator"><i class="material-icons right">more_vert</i></span>
-                <h6 class="truncate">Title: <a href="${latestNews[i].url}" title="${latestNews[i].title}">${latestNews[i].title}</a></h6>
-                <p><b>Author</b>: ${latestNews[i].author} </p>
-                <p><b>News source</b>: ${latestNews[i].source.name} </p>
-                <p><b>Published</b>: ${latestNews[i].publishedAt} </p>
-              </div>
-  
-              <div class="card-reveal">
-                <span class="card-title"><i class="material-icons right">close</i></span>
-                <p><b>Description</b>: ${latestNews[i].description}</p>
-              </div>
-  
-              <div class="card-action">
-                <a href="${latestNews[i].url}" target="_blank" class="btn">Read More</a>
-              </div>
-             </div>
-            </div>
-          `;
-        }
-  
-        if (output !== "") {
-          $("#newsResults").html(output);
-        }
-  
-      },
-  
-      error: function () {
-        let errorMsg = `<div class="errorMsg center">Some error occured</div>`;
-        $("#newsResults").html(errorMsg);
+            
+        
+        `;
+        count += 1
+        if(count == 2){
+          output += `</div>
+          </div>
+          </div>`
+          count = 0
+        };
       }
-    })
-  
-  });
+
+      if (output !== "") {
+        $("#newsResults").html(output);
+      }
+
+    },
+
+    error: function () {
+      let errorMsg = `<div class="errorMsg center">Some error occured</div>`;
+      $("#newsResults").html(errorMsg);
+    }
+  })
+
+});

2e23cc4c56267fec520cfe97084ef9e0dcc42ac6
ellayang0618 <66121210+ellayang0618@users.noreply.github.com>
2022-07-28 17:20:21 +0800

    changed gallery to photo of the day

--- public/static/js/home.js
+++ public/static/js/home.js
@@ -1,10 +1,7 @@
-
-function loadGalleryPage(){
-    console.log("gallery page is loaded")
+function loadHomePage(){
+    console.log("home page is loaded")
     sendApiRequest()
 }
-
-
 //An asynchronous function to fetch data from the API.
 async function sendApiRequest(){
     API_KEY = 'R5tVXdeOHcZbmXMTPknErP3hUNKdyfTxeQV1ZueN'
@@ -18,16 +15,15 @@ async function sendApiRequest(){
 
 
 function useApiData(data){
-    document.getElementById('title1').innerHTML = `<h1>${data.title}</h1>`;
-    document.getElementById('content1').innerText = data.explanation;
     if (data.media_type == 'image'){
-        
-        document.getElementById('source').innerHTML = `<img class="img-fluid" width="350" height="250" src="${data.url}" alt="">`;
         document.getElementById('home_photo').innerHTML = `<img class="img-fluid" width="350" height="250" src="${data.url}" alt="">`;
     }else{
-        document.getElementById('source').innerHTML = `<iframe width="500" height="350" src = "${data.url}"></iframe>`
+        document.getElementById('home_photo').innerHTML = `<iframe width="500" height="350" src = "${data.url}"></iframe>`
     
     }
+    document.getElementById('home_photo').onclick=function (){
+        window.location.href = "http://localhost/heap-g09/public/templates/gallery.html";
+    }
 
-    document.getElementById('date').innerText = data.date;
+    
 }
